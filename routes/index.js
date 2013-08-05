7ae760d6d4455468f80ce2f5af059c40ea4141cf
Daniel Castillo <dyland6@gmail.com>
2013-08-05 09:57:27 -0500

    upload corregido

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,13 @@ exports.index = function(req, res){
   res.render('index', { title: 'Libros Electronicos', mensaje:'Genera los archivos de texto solicitados por sunat' });
 };
 
-exports.upload = function(req, res){
+/*exports.upload = function(req, res){
   	var path = req.files.txtArchivo.path;
 	var nombre = req.files.txtArchivo.name;
 	console.log(path);
 	console.log(nombre);
 	var fileBytes = req.headers['content-length'];
+	console.log(fileBytes);
 	var uploadedBytes =0;
 	var archivo = fs.createWriteStream("./uploads/"+nombre);
 	req.pipe(archivo);
@@ -22,6 +23,23 @@ exports.upload = function(req, res){
 		var progress = (uploadedBytes / fileBytes)*100;	
 		res.write("progreso: "+ parseInt(progress,10));
 	});
-	
+	*/
+
+exports.upload = function(req, res){
+	var path = req.files.txtArchivo.path;
+	var nombre = req.files.txtArchivo.name;
+  	var ruta = "./uploads/"+nombre;
+  	fs.readFile(path,function(err, data){
+  		fs.writeFile(ruta,data,function(err){
+  			if(err){
+  				res.render('index', { title: 'Libros Electronicos', mensaje:'ha ocurrido un error' });
+  			} else{
+  				//validar formato de archivo
+  				res.render('index', { title: 'Libros Electronicos', mensaje:'se cargo el archivo '+nombre });
+  			}
+  			 
+  		});
+
+  	});
 };
 
